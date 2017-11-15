d0759d40a0d2196ef0c87f8a8f14fa239216a2c7
Truman Lahr <trumanlahr@gmail.com>
2017-11-15 23:17:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(input)
     "NO, NOT SINCE 1938!"
   elsif input == input.downcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif input == "I LOVE YOU GRANDMA!"
+  elsif input == 'I LOVE YOU GRANDMA!'
     "I LOVE YOU TOO PUMPKIN!"
   end
 end
